48654312ecb504d1db04c6a20e63ca342954eaf5
Giuseppe Soldatelli <soldatelligiuseppe@gmail.com>
2023-04-09 16:20:40 -0400

    first commit

---
+++ OrdenacaoPorSelecao/Ordenador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenacaoPorSelecao
+{
+	public class Ordenador
+	{
+		public int[] OrdenarArray(int[] array)
+		{
+			int tamanhoDoArray = array.Length;
+			int[] arrayOrdenado = new int[tamanhoDoArray];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				int indiceDoMenor = i;
+				for (int j = i + 1; j < array.Length; j++)
+				{
+					if (array[j] < array[indiceDoMenor])
+					{
+						indiceDoMenor = j;						
+					}
+				}
+
+				int menor = array[indiceDoMenor];
+				array[indiceDoMenor] = array[i];
+				arrayOrdenado[i] = menor;
+			}
+			return arrayOrdenado;
+		}
+	}
+}
---
+++ OrdenacaoPorSelecao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenacaoPorSelecao
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[] { 5, 4, 3, 2, 1};
+
+			var ordenador = new Ordenador();
+			var ordenado = ordenador.OrdenarArray(array);
+
+			string printOrdenado = string.Join(", ", ordenado);
+
+			Console.WriteLine("Array ordenado: [" + printOrdenado + "]");
+
+			Console.ReadLine();			
+		}
+	}
+}
